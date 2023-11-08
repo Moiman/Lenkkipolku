1dd695a19e093ea7c60ef6cdd5b237333b15efb0
Harri Nieminen <moiman@posteo.net>
2023-11-08 16:03:34 +0200

    tile-server: Enable noImplicitAny

--- tile-server/src/index.ts
+++ tile-server/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+// @ts-ignore
 import MBTiles from "@mapbox/mbtiles";
 import { requestLog, unknownEndpoint } from "./middleware.js";
 
@@ -16,12 +17,12 @@ const header = {
 
 // Route which handles requests like the following: /<mbtiles-name>/0/1/2.pbf
 app.get("/:z/:x/:y.pbf", (req, res) => {
-  new MBTiles("./data/tampere.mbtiles?mode=ro", (err, mbtiles) => {
+  new MBTiles("./data/tampere.mbtiles?mode=ro", (err: any, mbtiles: any) => {
     if (err) {
       console.error("error opening database");
       res.status(404).json({ error: "Error opening databes: " + err });
     }
-    mbtiles.getTile(req.params.z, req.params.x, req.params.y, (err, tile, _headers) => {
+    mbtiles.getTile(req.params.z, req.params.x, req.params.y, (err: any, tile: any, _headers: any) => {
       if (err) {
         res.status(404).json({ error: "Tile rendering error: " + err });
       } else {
