9a8bdab770ccf729f2a7f8afba72c7e13b42a952
Harri Nieminen <moiman@posteo.net>
2024-10-04 14:09:15 +0300

    tile-server: update to ESLint 9

--- tile-server/src/tilesRouter.ts
+++ tile-server/src/tilesRouter.ts
@@ -1,34 +1,44 @@
 import express from "express";
-// @ts-ignore
-import MBTiles from "@mapbox/mbtiles"; // No types
+// @ts-expect-error "No types"
+import MBTiles from "@mapbox/mbtiles";
 
 const router = express.Router();
 
 const header = {
   "Content-Type": "application/x-protobuf",
-  "Content-Encoding": "gzip"
+  "Content-Encoding": "gzip",
 };
 
 // Route which handles requests like the following: /0/1/2.pbf
 router.get("/:z/:x/:y.pbf", (req, res) => {
-  new MBTiles("./data/tampere.mbtiles?mode=ro", (err: any, mbtiles: any) => {
-    if (err) {
-      console.error("error opening database");
-      res.status(404).json({ error: "Error opening databes: " + err });
-    }
-    mbtiles.getTile(req.params.z, req.params.x, req.params.y, (err: any, tile: any, _headers: any) => {
+  new MBTiles(
+    "./data/tampere.mbtiles?mode=ro",
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (err: any, mbtiles: any) => {
       if (err) {
-        if (err.message === "Tile does not exist") {
-          res.status(204).end();
-        } else {
-          return res.status(500).json({ error: err.message });
-        }
-      } else {
-        res.set(header);
-        res.send(tile);
+        console.error("error opening database");
+        res.status(404).json({ error: "Error opening databes: " + err });
       }
-    });
-  });
+      mbtiles.getTile(
+        req.params.z,
+        req.params.x,
+        req.params.y,
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (err: any, tile: any) => {
+          if (err) {
+            if (err.message === "Tile does not exist") {
+              res.status(204).end();
+            } else {
+              return res.status(500).json({ error: err.message });
+            }
+          } else {
+            res.set(header);
+            res.send(tile);
+          }
+        }
+      );
+    }
+  );
 });
 
 export default router;
